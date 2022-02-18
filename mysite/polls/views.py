ff961cfc0db4337b1232bf7dcb2cf07b054a4dd9
Mateusz <m.klatka@gmail.com>
2022-02-18 20:43:28 +0100

    part 1 of part for of tutorial, old school views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,9 +3,10 @@ from multiprocessing import context
 from re import template
 from unittest import loader
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 from django.template import loader
 
 # Create your views here.
@@ -38,3 +39,29 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting at the question %s" % question_id)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #redisplay the question voting form
+        return render(request, 'polls:detail.html', {
+            'question':question,
+            'error_message':"You didn't select the choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+        #return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results (request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    
+    
+
+
+
+
